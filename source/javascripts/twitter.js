1458dc32867ca260e12e5b82e369ace2bf893534
shee-git <157801649+shee-git@users.noreply.github.com>
2024-03-29 15:11:58 +0900

    twitter.js 업데이트

--- source/javascripts/twitter.js
+++ source/javascripts/twitter.js
@@ -1,4 +1,1 @@
 // JSON-P Twitter fetcher for Octopress
-// (c) Brandon Mathis // MIT License
-
-/* Sky Slavin, Ludopoli. MIT license.  * based on JavaScript Pretty Date * Copyright (c) 2008 John Resig (jquery.com) * Licensed under the MIT license.  */
