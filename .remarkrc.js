450f796d2f6021b120c786d6efb0bbbe4e985d67
shee-git <157801649+shee-git@users.noreply.github.com>
2024-03-29 09:52:07 +0900

    .remarkrc.js 업데이트

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,10 +1,1 @@
-var remarkrc = {
-  settings: {
-    bullet: "-",
-    commonmark: true,
-    emphasis: "_",
-    fence: "`",
-    incrementListMarker: true,
-    listItemIndent: 1,
-    strong: "*",
-  }
+a
